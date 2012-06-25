a0f77c558d48b6a5b22e1bb61cc2721717de570f
Jon Rutherford <jrr66@uclive.ac.nz>
2012-06-25 22:37:04 +1200

    Add ability to link to a PT account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -22,18 +22,40 @@ class AccountsController < ApplicationController
     end
   end
 
-
   def create
+    errors = ""
     # check if we are creating a pt account
     if params[:type] == "pivotal_tracker"
-      user = user_from_auth_token
-      token = params[:token]
-      ptAccount = PtAccount.new(token)
-      user.accounts << ptAccount
-      user.save
+      if params[:email].nil? || params[:password].nil?
+        errors << "Email and password required."
+      else
+        create_pivotal_tracker
+        return
+      end
     else
-      # invalid type specified
-      render :json => {:error => I18n.t('request.bad_request')}, :status => :bad_request
+      errors << "Invalid account type."
+    end
+    # invalid type specified
+    render :json => {:error => "#{I18n.t('request.bad_request')}: #{errors}"}, :status => :bad_request
+  end
+
+
+  protected
+
+  def create_pivotal_tracker
+    begin
+      ptAccount = PtAccount.new(params[:email], params[:password])
+      user = user_from_auth_token
+      user.accounts << ptAccount
+      if (ptAccount.save!)
+        render :json => ptAccount, :status => :created, :location => url_for(controller: :accounts, action: :show, id: ptAccount.id)
+        return
+      else
+        render :json => {:error => "#{I18n.t('request.bad_request')}: #{ptAccount.errors}"}, :status => :bad_request
+      end
+    rescue RestClient::Unauthorized
+      render :json => {:error => "#{I18n.t('request.unauthorized')}: Incorrect email or password"}, :status => :unauthorized
     end
   end
+
 end
